a1d911a81698a50920eea18b9b4b1df01e039a48
John Doe <johndoe@example.com>
2022-06-20 17:09:37 +0000

    FIles: add created_at/by flags

--- models/Files.js
+++ models/Files.js
@@ -21,12 +21,13 @@ module.exports = (sequelize) => {
         id_auto: { type: dt.BIGINT, autoIncrement: true, primaryKey: true },
         id: { type: dt.STRING, unique: true, validate: validations.id },
         project_id: { type: dt.STRING },
-        uploader_id: { type: dt.STRING },
         directory: { type: dt.STRING },
         extension: { type: dt.STRING },
         mimetype: { type: dt.STRING },
         size: { type: dt.BIGINT },
-        upload_time: { type: dt.BIGINT },
+        //
+        created_at: { type: dt.BIGINT },
+        created_by: { type: dt.STRING },
         deleted_at: { type: dt.BIGINT },
         deleted_by: { type: dt.STRING },
     };
